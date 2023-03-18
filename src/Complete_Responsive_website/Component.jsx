948a7b689761a8adc06d66c1accd4a36b2c47ff0
Ashvani-Gurjar <singhashvani82@gmail.com>
2023-03-18 09:10:36 -0700

    second

--- src/Complete_Responsive_website/Component.jsx
+++ src/Complete_Responsive_website/Component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
+import './style.scss'
 const Common = (props) => {
      return (
           <>
